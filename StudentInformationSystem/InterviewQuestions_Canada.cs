6a197ea90edc438b348ce81558dce343b88dbda3
Yawar Khuwaja <yakhuwaj@microsoft.com>
2021-11-20 20:45:27 +0000

    with binary search now

--- StudentInformationSystem/InterviewQuestions_Canada.cs
+++ StudentInformationSystem/InterviewQuestions_Canada.cs
@@ -1,5 +1,7 @@
 ﻿namespace StudentInformationSystem
 {
+    using System.ComponentModel;
+
     public class InterviewQuestions_Canada
     {
         public int FindOneRepeatingNumberInAConsecutiveSequence(int[] arr)
@@ -19,12 +21,43 @@
         public int FindOneRepeatingNumberInAConsecutiveSequenceV2(int[] arr)
         {
             int i = 0;
-            while (i < arr.Length + 1 && arr[i] == arr[i + 1] -1)
+            while (i < arr.Length + 1 && arr[i] == arr[i + 1] - 1)
             {
                 i++;
             }
 
             return arr[i];
         }
+
+
+        /// <summary>
+        /// Binary Search
+        /// index = 0,1,2,3,4,5,6,7
+        /// value = 0,1,2,3,4,4,5,6
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public int FindOneRepeatingNumberInAConsecutiveSequenceV3(int[] arr)
+        {
+            int start = 0;
+            int end = arr.Length - 1;
+
+            while (start < end)
+            {
+                int midIndex = start + (end - start) / 2;
+
+                if (midIndex > arr[midIndex])
+                {
+                    end = midIndex - 1;
+                }
+                else
+                {
+                    start = midIndex + 1;
+                }
+            }
+
+            return arr[start];
+        }
+
     }
 }

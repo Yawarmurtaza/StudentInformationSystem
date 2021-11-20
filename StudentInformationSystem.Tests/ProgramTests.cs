916492de5abf752a69af3b5d122a4b812f4f6c20
Yawar Khuwaja <yakhuwaj@microsoft.com>
2021-11-20 13:06:16 +0000

    find the repeating numnber in a consecutive sequence

--- StudentInformationSystem.Tests/ProgramTests.cs
+++ StudentInformationSystem.Tests/ProgramTests.cs
@@ -1,10 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace StudentInformationSystem.Tests
+﻿namespace StudentInformationSystem.Tests
 {
     using NUnit.Framework;
+
+    [TestFixture]
+    public class InterviewQuestions_CanadaTests
+    {
+        private InterviewQuestions_Canada runner;
+        [SetUp]
+        public void Setup()
+        {
+            runner = new InterviewQuestions_Canada();
+        }
+
+        [TestCase(new[] { 0, 1, 2, 2, 3, 4 }, 2)]
+        [TestCase(new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10, 11 }, 9)]
+        [TestCase(new[] { 0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 10, 11, 12, 13, 14 }, 6)]
+        [TestCase(new[] { 0, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 }, 1)]
+        [TestCase(new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 14 }, 14)]
+        public void FindOneRepeatingNumberInAConsecutiveSequence(int[] arr, int expected)
+        {
+            int actual = runner.FindOneRepeatingNumberInAConsecutiveSequence(arr);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new[] { 0, 1, 2, 2, 3, 4 }, 2)]
+        [TestCase(new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10, 11 }, 9)]
+        [TestCase(new[] { 0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 10, 11, 12, 13, 14 }, 6)]
+        [TestCase(new[] { 0, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 }, 1)]
+        [TestCase(new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 14 }, 14)]
+        public void FindOneRepeatingNumberInAConsecutiveSequenceV2(int[] arr, int expected)
+        {
+            int actual = runner.FindOneRepeatingNumberInAConsecutiveSequenceV2(arr);
+            Assert.AreEqual(expected, actual);
+        }
+    }
 
     [TestFixture]
     public class ProgramTests
---
+++ StudentInformationSystem/InterviewQuestions_Canada.cs
@@ -0,0 +1,30 @@
+﻿namespace StudentInformationSystem
+{
+    public class InterviewQuestions_Canada
+    {
+        public int FindOneRepeatingNumberInAConsecutiveSequence(int[] arr)
+        {
+            int originalTotal = 0;
+            int total = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                originalTotal += i;
+                total += arr[i];
+            }
+
+            int index = originalTotal - total;
+            return arr[arr.Length - index];
+        }
+
+        public int FindOneRepeatingNumberInAConsecutiveSequenceV2(int[] arr)
+        {
+            int i = 0;
+            while (i < arr.Length + 1 && arr[i] == arr[i + 1] -1)
+            {
+                i++;
+            }
+
+            return arr[i];
+        }
+    }
+}
